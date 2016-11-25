9d19969e5eb369ac79e52576cb6827948c933bd9
C. Titus Brown <titus@idyll.org>
2016-11-25 13:02:58 -0600

    add argparse to characterize-mock

--- pipeline/characterize-mock.py
+++ pipeline/characterize-mock.py
@@ -5,28 +5,38 @@ characterize a "mock community" in terms of k-mer cardinality of each genome.
 import khmer
 import screed
 import re
+import argparse
 
-total_hll = khmer.HLLCounter(.01, 31)
-x = []
-for record in screed.open('15genome.fa.gz'):
-    hll = khmer.HLLCounter(.01, 31)
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('inp_fasta')
+    args = p.parse_args()
+    
+    total_hll = khmer.HLLCounter(.01, 31)
+    x = []
+    for record in screed.open(args.inp_fasta):
+        hll = khmer.HLLCounter(.01, 31)
 
-    seq = re.sub('[^ACGT]', 'A', record.sequence.upper())
-    hll.consume_string(seq)
+        seq = re.sub('[^ACGT]', 'A', record.sequence.upper())
+        hll.consume_string(seq)
 
-    print(record.name, hll.estimate_cardinality())
-    x.append((hll.estimate_cardinality(), record.name))
-    total_hll.merge(hll)
+        print(record.name, hll.estimate_cardinality())
+        x.append((hll.estimate_cardinality(), record.name))
+        total_hll.merge(hll)
 
-print('total', total_hll.estimate_cardinality())
-total = total_hll.estimate_cardinality()
+    print('total', total_hll.estimate_cardinality())
+    total = total_hll.estimate_cardinality()
 
-y = []
-for (c, n) in x:
-    y.append((c / total, n))
+    y = []
+    for (c, n) in x:
+        y.append((c / total, n))
 
-y.sort()
-y.reverse()
+    y.sort()
+    y.reverse()
 
-for c, n in y:
-    print('{:.2f} {}'.format(c, n))
+    for c, n in y:
+        print('{:.2f} {}'.format(c, n))
+
+
+if __name__ == '__main__':
+    main()
